e76a186acaae316b5ab13b810331e0214bb8a589
BoykoTut <165228397+BoykoTut@users.noreply.github.com>
2024-10-16 13:56:05 +1300

    Update presentation.py

--- presentation.py
+++ presentation.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
-st.title("Assignment 3 Presentation")
+# Set the title of the Streamlit app
+st.title("Presentation Slides")
 
 # Use the raw URL for the PDF
 pdf_url = "https://raw.githubusercontent.com/BoykoTut/Streamlit/5e339a92771c2c4be6bdfc29320fb9234fa92abf/Assignemnt%203%20Presentation.pdf"
 
-st.write("## Presentation Slides")
 
-# Embed the PDF using an iframe
-st.components.v1.iframe(pdf_url, width=700, height=800)
+# HTML code to embed the PDF
+pdf_embed_code = f"""
+<iframe src="{pdf_url}" width="700" height="800" style="border:none;"></iframe>
+"""
+
+# Use st.components.v1.html to render the HTML code
+st.components.v1.html(pdf_embed_code, height=800)
 
